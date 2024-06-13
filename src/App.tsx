caeff4fccc91f589830a3cb733fb44d2a27dd3c4
jisooround <9utesleben@gmail.com>
2024-06-13 15:10:02 +0900

    feat: 리액트 쿼리 설치 & provider 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import { RouterProvider } from "react-router-dom";
 import { routers } from "./router";
-import GlobalStyles from "./styles/GlobalStyles";
-import { RecoilRoot } from "recoil";
 
 function App() {
   return (
     <>
-      <RecoilRoot>
-        <GlobalStyles />
-        <RouterProvider router={routers} />
-      </RecoilRoot>
+      <RouterProvider router={routers} />
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import GlobalStyles from "./styles/GlobalStyles";
+import { RecoilRoot } from "recoil";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <GlobalStyles />
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </RecoilRoot>
+    </QueryClientProvider>
   </React.StrictMode>,
 );
