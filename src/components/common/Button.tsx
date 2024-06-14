c3153a1e1da6011f3ff50afa868beb49658e0c81
jisooround <9utesleben@gmail.com>
2024-06-14 17:34:05 +0900

    feat: button 컴포넌트 사이즈, 색상 props 전달

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,40 +1,65 @@
-import styled from "@emotion/styled";
 import { useNavigate } from "react-router-dom";
 import { goTo } from "../../utils/goTo";
+import { css } from "@emotion/react";
 
 type Props = {
   content: string;
   active?: boolean;
   path?: string;
+  size?: "sm" | "lg";
+  bg?: "lightgray" | "black" | "point";
+  bgHover?: "lightgray" | "black" | "point";
+  colorHover?: "white" | "black" | "point";
 };
 
-const Button = ({ content, active, path }: Props) => {
+const Button = ({ content, active = false, path = "", size = "sm", bg = "lightgray", bgHover = "point", colorHover = "black" }: Props) => {
   const navigate = useNavigate();
 
   return (
-    <ButtonStyle onClick={() => goTo(navigate, path)} active={active ?? false}>
+    <button css={size === "sm" ? SmallButton(active, bg, bgHover) : LargeButton(bg, bgHover, colorHover)} onClick={() => goTo(navigate, path)}>
       {content}
-    </ButtonStyle>
+    </button>
   );
 };
 
-const ButtonStyle = styled.button<{ active: boolean }>`
-  background-color: ${({ active }) => (active ? "var(--color-point)" : "var(--color-lightgray)")};
-  padding: 0 0.75rem;
-  font-size: 14px;
-  height: 30px;
-  border-radius: 6px;
-  line-height: 1.25rem;
-  text-transform: uppercase;
-  margin: 0 5px 5px 0;
-  outline: none;
-  border: ${({ active }) => (active ? "1px solid var(--color-point);" : "1px solid var(--color-lightgray);")};
-  cursor: pointer;
-  white-space: nowrap;
-  :hover {
-    background-color: var(--color-point);
-    border: 1px solid var(--color-point);
-  }
-`;
+const SmallButton = (active: boolean, bg: "lightgray" | "black" | "point", bgHover: "lightgray" | "black" | "point") =>
+  css({
+    backgroundColor: active ? "var(--color-point)" : "var(--color-lightgray)",
+    padding: "0 0.75rem",
+    fontSize: "14px",
+    height: "30px",
+    borderRadius: "6px",
+    lineHeight: "1.25rem",
+    textTransform: "uppercase",
+    margin: "0 5px 5px 0",
+    outline: "none",
+    border: active ? "1px solid var(--color-point)" : "1px solid var(--color-lightgray)",
+    cursor: "pointer",
+    whiteSpace: "nowrap",
+    ":hover": {
+      backgroundColor: `var(--color-${bgHover})`,
+      border: `1px solid var(--color-${bgHover})`,
+    },
+  });
 
+const LargeButton = (bg: "lightgray" | "black" | "point", bgHover: "lightgray" | "black" | "point", colorHover: "white" | "black" | "point") =>
+  css({
+    backgroundColor: `var(--color-${bg})`,
+    border: `1px solid var(--color-${bg})`,
+    padding: "0 1.125rem",
+    fontSize: "14px",
+    height: "65px",
+    width: "100%",
+    borderRadius: "0.375rem",
+    lineHeight: "1.25rem",
+    textTransform: "uppercase",
+    outline: "none",
+    cursor: "pointer",
+    whiteSpace: "nowrap",
+    ":hover": {
+      backgroundColor: `var(--color-${bgHover})`,
+      border: `1px solid var(--color-${bgHover})`,
+      color: `var(--color-${colorHover})`,
+    },
+  });
 export default Button;
