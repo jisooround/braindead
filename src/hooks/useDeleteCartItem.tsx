e90c77f155f12ef3bda30ee3f8a22b6d759aeb93
jisooround <9utesleben@gmail.com>
2024-06-18 22:23:17 +0900

    feat: 장바구니 > 장바구니 삭제 API 실행 성공시에 데이터 무효화& GetCartAPI 한번 더 실행

--- src/hooks/useDeleteCartItem.tsx
+++ src/hooks/useDeleteCartItem.tsx
@@ -1,12 +1,15 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteCartItem } from "../api/api";
 import { DeleteCartItem } from "../types/cart";
 
 function useDeleteCartItem() {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: (deleteData: DeleteCartItem) => deleteCartItem(deleteData),
     onSuccess: (data) => {
       console.log(data);
+      queryClient.invalidateQueries({ queryKey: ["myCart"] });
     },
     onError: () => {
       alert("장바구니 삭제 실패");
