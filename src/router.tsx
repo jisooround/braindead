57e36aac6e9c2c0d884d2f0e93a929e7be364f2d
jisooround <9utesleben@gmail.com>
2024-06-19 20:37:46 +0900

    feat: 라우터 > 객체 통일

--- src/router.tsx
+++ src/router.tsx
@@ -16,61 +16,63 @@ import Cart from "./pages/Cart";
 import TopList from "./pages/Products/TopList";
 import AllProducts from "./pages/Products/AllProducts";
 
-interface RouterBase {
+interface Router {
   id: number; // 페이지 아이디 (반복문용 고유값)
   path: string; // 페이지 경로
   label: string; // 사이드바에 표시할 페이지 이름
   element: React.ReactNode; // 페이지 엘리먼트
+  isGuestOnly: boolean; // 비회원 여부
 }
 
-interface UserAccessibleRouterElement extends RouterBase {
-  isGuestOnly?: boolean; // 손님전용 페이지 여부
-}
-
-type RouterElement = UserAccessibleRouterElement;
-
-export const routerData: RouterElement[] = [
+export const routerData: Router[] = [
   {
     id: 0,
     path: "/",
     label: "home",
     element: <Home />,
+    isGuestOnly: false,
   },
   {
     id: 1,
     path: "/pages/terms-of-service",
     label: "terms",
     element: <Terms />,
+    isGuestOnly: false,
   },
   {
     id: 2,
     path: "/pages/refund-policy",
     label: "refund-policy",
     element: <RefundPolicy />,
+    isGuestOnly: false,
   },
   {
     id: 3,
     path: "/pages/privacy-policy",
     label: "privacy-policy",
     element: <PrivacyPolicy />,
+    isGuestOnly: false,
   },
   {
     id: 4,
     path: "/pages/about-us",
     label: "about-us",
     element: <AboutUs />,
+    isGuestOnly: false,
   },
   {
     id: 5,
     path: "/pages/stores",
     label: "stores",
     element: <Stores />,
+    isGuestOnly: false,
   },
   {
     id: 6,
     path: "/pages/contact",
     label: "contact",
     element: <Contact />,
+    isGuestOnly: false,
   },
   {
     id: 7,
@@ -91,18 +93,21 @@ export const routerData: RouterElement[] = [
     path: "/cart",
     label: "cart",
     element: <Cart />,
+    isGuestOnly: false,
   },
   {
     id: 9,
     path: "/collections/all-products",
     label: "all-products",
     element: <AllProducts />,
+    isGuestOnly: false,
   },
   {
     id: 10,
     path: "/collections/top",
     label: "top",
     element: <TopList />,
+    isGuestOnly: false,
   },
 ];
 
@@ -110,7 +115,7 @@ export const routers: RemixRouter = createBrowserRouter(
   routerData.map((router) => {
     return {
       path: router.path,
-      element: <GeneralLayout isGuestOnly={"isGuestOnly" in router && router.isGuestOnly}>{router.element}</GeneralLayout>,
+      element: <GeneralLayout isGuestOnly={router.isGuestOnly}>{router.element}</GeneralLayout>,
       errorElement: <NotFound />,
     };
   }),
