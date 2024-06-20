5efc7bc58631c891207cf9d2bc24021d2d4d6a77
jisooround <9utesleben@gmail.com>
2024-06-20 18:32:27 +0900

    feat: 장바구니 추가 시 getCart 실행

--- src/hooks/useAddCartItem.tsx
+++ src/hooks/useAddCartItem.tsx
@@ -1,12 +1,15 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { addCartItem } from "../api/api";
 import { CartData } from "../types/cart";
 
 function useAddCartItem() {
+  const queryClient = useQueryClient();
+
   return useMutation({
     mutationFn: (cartData: CartData) => addCartItem(cartData),
     onSuccess: (data) => {
       console.log(data);
+      queryClient.invalidateQueries({ queryKey: ["myCart"] });
     },
     onError: () => {
       alert("장바구니 추가 실패");
