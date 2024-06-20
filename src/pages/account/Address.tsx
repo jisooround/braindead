d338f3f265f4188e99340c226dcda4e1e4256087
jisooround <9utesleben@gmail.com>
2024-06-20 18:32:58 +0900

    feat: address 페이지

--- src/pages/account/Address.tsx
+++ src/pages/account/Address.tsx
@@ -1,7 +1,24 @@
 import styled from "@emotion/styled";
+import { useRecoilState } from "recoil";
+import { authTokenState } from "../../recoil/atoms/authAtom";
 
 const Address = () => {
-  return <AddressContainer>Address</AddressContainer>;
+  const [authState, setAuthState] = useRecoilState(authTokenState);
+  console.log(authState);
+  return (
+    <AddressContainer>
+      <h2>YOUR ADDRESSES</h2>
+      <AddressWrap>
+        <div>
+          {Object.entries(authState?.user).map(([key, value]) => {
+            return <p>{value}</p>;
+          })}
+          <p>{authState?.user?.username}</p>
+          <p>{authState?.user?.company}</p>
+        </div>
+      </AddressWrap>
+    </AddressContainer>
+  );
 };
 
 const AddressContainer = styled.div`
@@ -16,6 +33,10 @@ const AddressContainer = styled.div`
     margin-bottom: 32px;
     text-transform: uppercase;
   }
+`;
+
+const AddressWrap = styled.div`
+  border: 1px dashed var(--color-black);
 `;
 
 export default Address;
