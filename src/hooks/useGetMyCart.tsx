26d55c309c8bc3c80bd4607e068061b76db6dfa3
jisooround <9utesleben@gmail.com>
2024-06-24 00:54:17 +0900

    chore: 장바구니 useQuery 수정

--- src/hooks/useGetMyCart.tsx
+++ src/hooks/useGetMyCart.tsx
@@ -6,9 +6,8 @@ import { useRecoilValue } from "recoil";
 function useGetMyCart() {
   const authState = useRecoilValue(authTokenState);
   return useQuery({
-    queryKey: ["myCart", authState?.token], // query키는 어떻게 결정할까? - 이 데이터가 무슨 데이터인지 알려줌(=myCart), - 이 데이터가 어떤 값에 의존해서 변하는지 알려줌(token=user)
+    queryKey: ["myCart", authState?.token],
     queryFn: () => getMyCart(),
-    enabled: Boolean(authState?.token),
   });
 }
 
