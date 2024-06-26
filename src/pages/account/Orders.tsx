4de14d2cbf2ed1b4aeaccfa6727910ff7c4a2030
jisooround <9utesleben@gmail.com>
2024-06-26 22:20:31 +0900

    feat: 오더히스토리 > UI 완성

--- src/pages/account/Orders.tsx
+++ src/pages/account/Orders.tsx
@@ -1,33 +1,76 @@
 import styled from "@emotion/styled";
 import useGetOrderHistory from "../../hooks/useGetOrderHistory";
+import { useState } from "react";
+import { formatPrice } from "../../utils/formatPrice";
+import Button from "../../components/common/Button";
 
 const Orders = () => {
   const { isPending, error, data: orderHistory } = useGetOrderHistory();
+  const [isOpen, setIsOpen] = useState<number>(null);
 
   if (isPending) return <p>Loading...</p>;
   return (
     <OrdersContainer>
+      <h2>YOUR ORDER HISTORY</h2>
       {orderHistory.orders.map((order) => {
         return (
-          <div>
-            <p>total price : {order.total_price}</p>
-            <p>order id : {order.id}</p>
-            <p>order item</p>
-            {order.items.map((item) => {
-              return (
-                <div>
-                  <p>item quantity : {item.quantity}</p>
-                  {/* {item.product.map((product) => {
-                    return (
-                      // <div>
-                      //   <p>product name : {product.name}</p>
-                      // </div>
-                    );
-                  })} */}
-                </div>
-              );
-            })}
-          </div>
+          <OrderBox>
+            <OrderWrap>
+              <div>
+                <p>ORDER ID</p>
+                <span>{order.id}</span>
+              </div>
+              <div>
+                <p>TOTAL PRICE</p>
+                <span>₩ {formatPrice(order.total_price)}</span>
+              </div>
+              <div>
+                <p>MEMO</p>
+                <span>{order.memo}</span>
+              </div>
+            </OrderWrap>
+            {isOpen === order.id && (
+              <OrderDetailWrap>
+                {order.items.map((item) => {
+                  return (
+                    <OrderProductWrap key={item.product.name}>
+                      <ImageArea>
+                        <img src={item.product.photos[0]} alt="" />
+                      </ImageArea>
+                      <InfoArea>
+                        <p>{item.product.name}</p>
+                        <p>SIZE: {item.size}</p>
+                        <p>QUANTITY: {item.quantity}</p>
+                        <p>PRICE: {item.product.price}</p>
+                      </InfoArea>
+                    </OrderProductWrap>
+                  );
+                })}
+              </OrderDetailWrap>
+            )}
+            {isOpen === order.id && (
+              <Button
+                content="close"
+                size="sm"
+                bg="point"
+                bgHover="black"
+                onClick={() => {
+                  setIsOpen(null);
+                }}
+              />
+            )}
+            {isOpen !== order.id && (
+              <Button
+                content="view"
+                size="sm"
+                bg="point"
+                bgHover="black"
+                onClick={() => {
+                  setIsOpen(order.id);
+                }}
+              />
+            )}
+          </OrderBox>
         );
       })}
     </OrdersContainer>
@@ -36,8 +79,8 @@ const Orders = () => {
 
 const OrdersContainer = styled.div`
   max-width: 460px;
-  height: calc(100vh - 158px);
-  margin: 158px auto 0;
+  height: auto;
+  margin: 158px auto;
   h2 {
     width: auto;
     font-size: 22px;
@@ -46,6 +89,72 @@ const OrdersContainer = styled.div`
     margin-bottom: 32px;
     text-transform: uppercase;
   }
+`;
+
+const OrderBox = styled.div`
+  width: 100%;
+  border: 1px dashed var(--color-black);
+  border-radius: 0.375rem;
+  padding: 16px;
+  margin-bottom: 16px;
+  button {
+    width: 100%;
+    margin: 0.5rem 0 0 0;
+  }
+`;
+
+const OrderWrap = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  font-size: 14px;
+  div {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 4px;
+    span {
+      font-weight: 400;
+    }
+  }
+`;
+const OrderDetailWrap = styled.div`
+  width: 100%;
+  border-top: 1px dashed var(--color-black);
+  margin-top: 1rem;
+  padding-top: 1rem;
+  padding-bottom: 0.5rem;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`;
+
+const OrderProductWrap = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 2fr 8fr;
+  background-color: var(--color-lightgray);
+  padding: 0.5rem;
+  box-sizing: border-box;
+  border-radius: 0.375rem;
+`;
+
+const ImageArea = styled.div`
+  width: 100%;
+  object-fit: contain;
+  border-radius: 0.375rem;
+  overflow: hidden;
+  img {
+    width: 100%;
+  }
+`;
+
+const InfoArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-left: 0.5rem;
+  padding: 0.5rem;
+  font-size: 14px;
+  gap: 0.5rem;
 `;
 
 export default Orders;
