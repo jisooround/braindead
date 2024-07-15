2da2df7303a5b54159fbbd8ce2766a62ad636202
jisooround <9utesleben@gmail.com>
2024-07-15 22:26:59 +0900

    chore: 불필요한 import 삭제

--- src/hooks/useSearch.tsx
+++ src/hooks/useSearch.tsx
@@ -1,5 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { getProductDetails, searchProducts } from "../api/api";
+import { searchProducts } from "../api/api";
 
 export default function useSearch(params) {
   return useQuery({ queryKey: ["search", params], queryFn: () => searchProducts(params) });
