ed5710a909c35547ee9a6eb2f26495bc0a6e1d32
jisooround <9utesleben@gmail.com>
2024-07-15 22:28:36 +0900

    feat: 상품목록 더보기 구현 (카테고리)

--- src/pages/Products/CategoryProducts.tsx
+++ src/pages/Products/CategoryProducts.tsx
@@ -1,13 +1,46 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import Button from "../../components/common/Button";
 import ProductsList from "../../components/products/ProductsList";
 import useGetCategory from "../../hooks/useGetCategoryItem";
 import styled from "@emotion/styled";
 import SkeletonProductsList from "../../components/products/SkeletonProductsList";
+import useURLParams from "../../hooks/useURLParams";
+import { useEffect, useState } from "react";
+import { goTo } from "../../utils/goTo";
+import useGetProductsList from "../../hooks/useGetProductsList";
 
 const CategoryProducts = () => {
   const { category } = useParams();
-  const { data, error, isPending } = useGetCategory(category);
+  const query = useURLParams();
+  const pagination = query.get("pagination");
+  const navigate = useNavigate();
+  const [categoryName, setCategoryName] = useState(category);
+  const [page, setPage] = useState<number>(pagination ? Number(pagination) : 1);
+  const { isPending, data } = useGetProductsList(category, 12 * Number(page));
+
+  // 카테고리 이름 setState
+  useEffect(() => {
+    setCategoryName(category);
+  }, []);
+
+  // page 이름 변경되면 url에 pagination 정보 추가
+  useEffect(() => {
+    if (page !== 1) {
+      goTo(navigate, `/products/${category}?pagination=${page}`);
+    }
+  }, [page]);
+
+  // 1. 같은 카테고리에서 새로고침할 경우 url 유지
+  // 2. 다른 카테고리로 이동할 경우 page 1로 setState
+  useEffect(() => {
+    if (category === categoryName) return;
+    setPage(1);
+    return () => {
+      if (page !== 1) {
+        goTo(navigate, `/products/${category}`);
+      }
+    };
+  }, [category]);
 
   return (
     <CategoryProductsContainer>
@@ -16,6 +49,12 @@ const CategoryProducts = () => {
         <p>List of all Brain Dead apparel ordered by date, newest first.</p>
       </TitleWrap>
       <ResultItemWrap>{isPending ? <SkeletonProductsList /> : <ProductsList listData={data} />}</ResultItemWrap>
+      <Button
+        content="more"
+        onClick={() => {
+          setPage((prev) => prev + 1);
+        }}
+      />
     </CategoryProductsContainer>
   );
 };
